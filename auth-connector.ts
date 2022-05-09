2229f69a0d19b6ea82e2a5286220dfb9b07a23ec
Luca Kiebel <github@luca-kiebel.de>
2022-05-09 21:45:52 +0200

    add register functionality

--- auth-connector.ts
+++ auth-connector.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 const keycloakUrl = process.env.KEYCLOAK_URL!;
 const keycloakRealm = process.env.KEYCLOAK_REALM!;
 const keycloakClient = process.env.KEYCLOAK_CLIENT!;
+const userServiceUrl = process.env.USER_SERVICE_URL!;
 
 const login = async (username: string, password: string) => {
     const params = new URLSearchParams();
@@ -12,14 +13,38 @@ const login = async (username: string, password: string) => {
     params.append('client_id', keycloakClient);
     params.append('username', username);
     params.append('password', password);
+    const response = await axios.post(
+      `${keycloakUrl}/realms/${keycloakRealm}/protocol/openid-connect/token`,
+      params,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+
+    axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.access_token}`;
+
+    return response.data;
+};
+
+const register = async (email:string, password:string, firstName:string, lastName:string, onboardingToken:string) => {
+  const data = {
+    email,
+    password,
+    firstName,
+    lastName,
+    onboardingToken,
+  }
   const response = await axios.post(
-    `${keycloakUrl}/realms/${keycloakRealm}/protocol/openid-connect/token`,
-    params,
+    `${userServiceUrl}/users/register`,
+    data,
     {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json',
       },
-    },
+    }
   );
+
   return response.data;
-};
+}
